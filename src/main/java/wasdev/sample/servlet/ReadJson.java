e80136015e37ac29d9b4f1b2b87cf5769222c5aa
Moorels Alexis <moorels@fr.ibm.com>
2017-04-04 05:09:34 -0500

    connexion api

---
+++ src/main/java/wasdev/sample/servlet/ReadJson.java
@@ -0,0 +1,16 @@
+package wasdev.sample.servlet;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+  
+public class ReadJson {
+	
+	
+	
+	}
--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -22,6 +22,20 @@ public class SimpleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().print("Hello test World!");
+        
+        VisualRecognition service = new VisualRecognition();
+		service.setAPIKey("{
+  							"url": "https://gateway-a.watsonplatform.net/visual-recognition/api",
+  							"note": "It may take up to 5 minutes for this key to become active",
+							"api_key": "be542628436545954bf7f34c3669ccf89e218bb0"
+						    "}");
+		System.out.println("Classify an image");
+		ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
+    	.images(new File("src/image1081.jpg"))
+    	.build();
+		VisualClassification result = service.classify(options).execute();
+		System.out.println(result);				    
+						    
     }
 
 }
